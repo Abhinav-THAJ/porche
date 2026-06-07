8fd47591fe52213bb3b8d9a06e4e10493b2eaafb
Abhinav-THAJ <98870669+Abhinav-THAJ@users.noreply.github.com>
2026-06-07 19:08:47 +0530

    feat: increase Lenis smooth scroll inertia and vertically stack mobile buttons

--- components/SmoothScrolling.tsx
+++ components/SmoothScrolling.tsx
@@ -4,7 +4,7 @@ import { ReactLenis } from "lenis/react";
 
 export default function SmoothScrolling({ children }: { children: React.ReactNode }) {
   return (
-    <ReactLenis root options={{ lerp: 0.05, duration: 1.5, smoothWheel: true }}>
+    <ReactLenis root options={{ lerp: 0.03, duration: 2.5, smoothWheel: true, wheelMultiplier: 0.8, touchMultiplier: 2 }}>
       {children}
     </ReactLenis>
   );
