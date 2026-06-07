90a4b61e3121fb7dede40f9192085244ed7fd93b
Abhinav-THAJ <98870669+Abhinav-THAJ@users.noreply.github.com>
2026-06-07 19:18:15 +0530

    chore: push smooth scroll physics to extreme maximum floatiness

--- components/SmoothScrolling.tsx
+++ components/SmoothScrolling.tsx
@@ -4,7 +4,7 @@ import { ReactLenis } from "lenis/react";
 
 export default function SmoothScrolling({ children }: { children: React.ReactNode }) {
   return (
-    <ReactLenis root options={{ lerp: 0.03, duration: 2.5, smoothWheel: true, wheelMultiplier: 0.8, touchMultiplier: 2 }}>
+    <ReactLenis root options={{ lerp: 0.015, duration: 3.5, smoothWheel: true, wheelMultiplier: 0.6, touchMultiplier: 3 }}>
       {children}
     </ReactLenis>
   );
